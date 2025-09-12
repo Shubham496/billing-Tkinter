2a1a0b515b12cf14cd4f74bb2ae2922ed5ac51f7
Shubham <iatco5h@gmail.com>
2025-09-12 13:47:24 +0530

    billing

--- billing.py
+++ billing.py
@@ -9,6 +9,11 @@ center = WINDOW_SIZE_X//2
 
 
 
+def genrate_bill():
+    bill_text = ""
+    for i in LABLES:
+        print(i)
+
 
 
 
@@ -41,9 +46,23 @@ myTitle.place(x=center - (len(SOFTWARE_TITLE)*6//2) , y=20)
 LABLES = {}
 
 
+
+
+
+
 Namkeen = tk.Label(text = "Namkeen", font=("arial", 12, "bold"))
 Q_Namkeen = tk.Entry()
 LABLES["Namkeen"]  = [Namkeen, 50, Q_Namkeen]
+
+
+
+
+
+
+
+
+
+
 
 SoftDrink = tk.Label(text = "SoftDrink", font=("arial", 12, "bold"))
 Q_SoftDrink = tk.Entry()
@@ -58,14 +77,37 @@ Q_Chocolates = tk.Entry()
 LABLES["Chocolates"]  = [Chocolates, 100, Q_Chocolates]
 
 
+
+
+
+B_Total = tk.Button(text="Calculate Bill", font=("arial", 12), command=genrate_bill)
+B_Total.place(x=250, y=250)
+
+
+
+
+
+
 Lable_position_x = 12
 Lable_position_y = 50
 increment = 0
 
 for i in LABLES:
     #print(LABLES[i])
+    # LABELS have Lable object at index 0
+    # LABELS have price at index 1
+    # Lables Have entry at index 2
+
+    # placing the lables at position according to x and y axies
     LABLES[i][0].place(x=Lable_position_x, y=Lable_position_y + increment)
+
+    # placing the price at position accoding to x and y
     tk.Label(text=f"₹ {LABLES[i][1]}", font=("arial", 10)).place(x=Lable_position_x + 150,  y=Lable_position_y + increment)
+    
+    # placing the Entry at position accoding to x and y
+    LABLES[i][2].place(x=Lable_position_x + 250, y=Lable_position_y + increment)
+    
+    # placing every item after 30 pixels below the preceding 
     increment += 30
 
 # running the window
