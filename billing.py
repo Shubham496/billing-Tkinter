3e807fce7313cb26bd958b100c444255a92f4eaf
Shubham <iatco5h@gmail.com>
2025-09-12 13:16:03 +0530

    applicaiton

--- billing.py
+++ billing.py
@@ -8,27 +8,6 @@ center = WINDOW_SIZE_X//2
 
 
 
-def getBill():
-    
-    price = [45, 100, 50, 20]
-
-    a = E_SOFTDRINK.get() or 0
-    b = E_CHOCOLATE.get() or 0
-    c = E_NAMKEEN.get() or 0
-    d = E_CHIPS.get() or 0
-
-    items = [a,b,c,d]
-    total = 0
-    for i in range(len(items)):
-        if str(items[i]).isnumeric():
-            total += int(items[i]) * price[i]
-
-    print(total)    
-
-
-
-
-
 
 
 
@@ -55,45 +34,38 @@ root.resizable(True, False)
 
 SOFTWARE_TITLE = "BILLING SYSTEM"
 myTitle = tk.Label(text=SOFTWARE_TITLE)
-myTitle.place(x=center-50 , y=10)
+myTitle.place(x=center - (len(SOFTWARE_TITLE)*6//2) , y=20)
 
 
 
+LABLES = {}
 
 
+Namkeen = tk.Label(text = "Namkeen", font=("arial", 12, "bold"))
+LABLES["Namkeen"]  = [Namkeen, 50]
+
+SoftDrink = tk.Label(text = "SoftDrink", font=("arial", 12, "bold"))
+LABLES["SoftDrink"]  = [SoftDrink, 40]
+
+Chips = tk.Label(text = "Chips", font=("arial", 12, "bold"))
+LABLES["Chips"]  = [Chips, 20]
+
+Chocolates = tk.Label(text = "Chocolates", font=("arial", 12, "bold"))
+LABLES["Chocolates"]  = [Chocolates, 100]
 
 
+Toothpaste = tk.Label(text = "Toothpaste", font=("arial", 12, "bold"))
+LABLES["Toothpaste"]  = [Toothpaste, 100]
 
 
+Lable_position_x = 12
+Lable_position_y = 50
+increment = 0
 
-L_SOFTDRINK = tk.Label(root, text= 'SoftDrink', font=("arial")).place(x=20, y=40)
-L_PRICE_SOFTDRINK = tk.Label(root, text="₹ 45", font=("arial")).place(x=200, y=40)
-E_SOFTDRINK = tk.Entry(root)
-E_SOFTDRINK.place(x=300, y=40)
-
-
-L_CHOCOLATE = tk.Label(root, text= 'Chocolate', font=("arial")).place(x=20, y=70)
-L_PRICE_CHOCOLATE = tk.Label(root, text="₹ 100", font=("arial")).place(x=200, y=70)
-E_CHOCOLATE = tk.Entry(root)
-E_CHOCOLATE.place(x=300, y=70)
-
-L_NAMKEEN = tk.Label(root, text= 'NAMKEEN', font=("arial")).place(x=20, y=100)
-L_PRICE_NAMKEEN = tk.Label(root, text="₹ 50", font=("arial")).place(x=200, y=100)
-E_NAMKEEN = tk.Entry(root)
-E_NAMKEEN.place(x=300, y=100)
-
-L_CHIPS = tk.Label(root, text= 'CHIPS', font=("arial")).place(x=20, y=130)
-L_PRICE_CHIPS = tk.Label(root, text="₹ 20", font=("arial")).place(x=200, y=130)
-E_CHIPS = tk.Entry(root)
-E_CHIPS.place(x=300, y=130)
-
-
-
-
-
-CalculateBill = tk.Button(text="Calculate Bill", font=("arial"), command=getBill).place(x=300, y=230)
-
-
+for i in LABLES:
+    print(LABLES[i])
+    LABLES[i][0].place(x=Lable_position_x, y=Lable_position_y + increment)
+    increment += 30
 
 # running the window
 root.mainloop()
